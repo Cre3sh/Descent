84a862a3d97399119a5c5b52dc16729c9b0cd254
Cre3sh <56470551+Cre3sh@users.noreply.github.com>
2024-09-16 18:32:58 +0100

    Delete Source/DescentTheGame/Private/UI directory

--- Source/DescentTheGame/Private/UI/UI/Player/DCUIActionPromptWidget.cpp
+++
@@ -1,8 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "UI/Player/DCUIActionPromptWidget.h"
-
-void UDCUIActionPromptWidget::SetActionText(FText ActionText) const
-{
-	ActionTextBlock->SetText(ActionText);
-}
